2df60b9ab4866d7fa4140d4b7e278d9bd5c33449
Sathaporn-Phila <nuinee_2544@hotmail.com>
2021-04-30 15:21:56 +0700

    Add app in q2 urls

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from .models import Question
 # Create your views here.
 def display(request):
     return render(request,"polls/index.html")
